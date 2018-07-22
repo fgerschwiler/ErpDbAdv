30ca9377a3e78de9f19087d1be04a2ea832c0b39
Benjamin Schaeublin <benjamin.schaeublin@cmiag.ch>
2018-07-22 23:41:38 +0200

    database first  (first steps)
    create classes from dbmodel

--- ZBW.BPFM.DBAdv.ErpClient/ZBW.BPFM.DBAdv.DBAccess/Class1.cs
+++ ZBW.BPFM.DBAdv.ErpClient/ZBW.BPFM.DBAdv.DBAccess/Class1.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ZBW.BPFM.DBAdv.DBAccess
 {
-    public class Class1
+    public class AuftragRepository
     {
+        public IQueryable<Bestellung> GetAuftraegeAsQueryable()
+        {
+            using (var ctx = new ErpContext())
+            {
+                return ctx.Bestellungen.AsQueryable();
+            }
+        }
+
+        public Task<Bestellung> GetBestellungAsync(int id)
+        {
+            using (var ctx = new ErpContext())
+            {
+                return ctx.Bestellungen.FindAsync(id);
+            }
+        }
+
+        public async Task<Bestellung> CreateBestellungAsync(Bestellung b)
+        {
+            using (var ctx = new ErpContext())
+            {
+                var newBestellung = ctx.Bestellungen.Add(b);
+                await ctx.SaveChangesAsync();
+                return newBestellung;
+            }
+        }
+
+
+        public async Task DeleteBestellungAsync(Bestellung b)
+        {
+            using (var ctx = new ErpContext())
+            {
+                ctx.Bestellungen.Remove(b);
+                await ctx.SaveChangesAsync();
+            }
+        }
     }
 }
