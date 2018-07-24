15e58d621ab52966269b8dbf16b5b43db569f818
Benjamin Schaeublin <benjamin.schaeublin@cmiag.ch>
2018-07-24 23:17:15 +0200

    some fixes to view

---
+++ ZBW.BPFM.DBAdv.ErpClient/ZBW.BPFM.DBAdv.DBAccess/kunde.partial.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZBW.BPFM.DBAdv.DBAccess
+{
+    public partial class kunde
+    {
+        public string DisplayName => this.person.Vorname + " " + this.person.Name;
+    }
+}
