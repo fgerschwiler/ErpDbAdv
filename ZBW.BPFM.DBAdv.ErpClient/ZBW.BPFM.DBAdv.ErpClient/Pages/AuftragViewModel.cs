2ab0d00c971b4e6ca7928a465ad2b4fed10cb8bd
Benjamin Schaeublin <benjamin.schaeublin@cmiag.ch>
2018-08-18 17:09:09 +0200

    allowing filter by id

--- ZBW.BPFM.DBAdv.ErpClient/ZBW.BPFM.DBAdv.ErpClient/Pages/AuftragViewModel.cs
+++ ZBW.BPFM.DBAdv.ErpClient/ZBW.BPFM.DBAdv.ErpClient/Pages/AuftragViewModel.cs
@@ -1,22 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using ZBW.BPFM.DBAdv.DBAccess;
+using ZBW.BPFM.DBAdv.ErpClient.Annotations;
 
 namespace ZBW.BPFM.DBAdv.ErpClient.Pages
 {
-    public class AuftragViewModel
+    public class AuftragViewModel : INotifyPropertyChanged
     {
         private AuftragRepository _repository = null;
-        public BindingList<bestellung> Auftraege { get; set; }
+        private string _searchFilter;
+        public ObservableCollection<bestellung> FilteredAuftraege { get; set; }
+
+        public string SearchFilter
+        {
+            get { return _searchFilter; }
+            set
+            {
+                _searchFilter = value;
+                this.FilteredAuftraege = new ObservableCollection<bestellung>(_repository.GetAuftraegeAsync().Where(a => a.Id.ToString().Contains(value)));
+                OnPropertyChanged(nameof(FilteredAuftraege));
+            }
+        }
+
         public AuftragViewModel()
         {
             _repository = new AuftragRepository();
             var auftraege = _repository.GetAuftraegeAsync();
-            Auftraege = new BindingList<bestellung>(auftraege);
+            FilteredAuftraege = new ObservableCollection<bestellung>(auftraege);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
